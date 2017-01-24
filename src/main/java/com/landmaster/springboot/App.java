cdf9a8b93326c3d6b0cab6f5b13ef657bcf90feb
tong0711 <tong0711@163.com>
2017-01-24 13:13:03 +0800

    初始化提交

---
+++ src/main/java/com/landmaster/springboot/App.java
@@ -0,0 +1,29 @@
+package com.landmaster.springboot;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+//@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World my frist for spring cloud!";
+    }
+
+    public static void main(String[] args) throws Exception {
+    	
+        SpringApplication.run(App.class, args);
+    }
+
+
+}
