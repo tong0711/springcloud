e532e9cb95db8da8a3d82676bcaed7ee15c83dca
tong0711 <tong0711@163.com>
2017-01-25 09:20:20 +0800

    暂时去掉了consul的配置

--- src/main/java/com/landmaster/springboot/App.java
+++ src/main/java/com/landmaster/springboot/App.java
@@ -5,13 +5,13 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 //@Controller
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @RestController
 public class App {
     @Value("spring.cloud.consul.host")
